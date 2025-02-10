6a51672b835726302ca90d096c15daf12149193a
Sanjida Rima <sanjidarima.drive@gmail.com>
2025-02-10 15:11:12 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.hero_banner_bg').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+	items:1,
+	autoplay:true,
+    autoplayTimeout:2000,
+	
+    
+});
+
+$('.testimonial_slide').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+	items:2,
+	autoplay:true,
+    autoplayTimeout:2000,
+	
+	
+    
+});
